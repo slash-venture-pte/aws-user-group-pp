fb1908aa49a3cdf2a19bfb162805e425933f4234
Sean Nuon <sean@slash.co>
2022-08-30 16:21:57 +0700

    update favicon and logo

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { Background } from '../background/Background';
 import { Button } from '../button/Button';
@@ -7,37 +8,43 @@ import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
-const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>RSVP Now</a>
-          </Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
+const Hero = () => {
+  const router = useRouter();
+  return (
+    <Background color="bg-gray-100">
+      <Section yPadding="py-6">
+        <NavbarTwoColumns logo={<Logo xl />}>
+          <a href="https://slash.co" target={'_blank'} rel="noreferrer">
+            <img
+              width={'65'}
+              src={`${router.basePath}${'/assets/images/slash-logo-black.png'}`}
+              alt={'Slash Digital'}
+              className={'position-relative ml-2 '}
+            />{' '}
+          </a>
+        </NavbarTwoColumns>
+      </Section>
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {"You're invited to AWS Community Day Phnom Penh 2022 \n"}
-            <span className="text-primary-500">+ AWS DeepRacer</span>
-          </>
-        }
-        description="October 16th, 2022"
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <a>
-              <Button xl>Register your RSVP Here</Button>
-            </a>
-          </Link>
-        }
-      />
-    </Section>
-  </Background>
-);
+      <Section yPadding="pt-20 pb-32">
+        <HeroOneButton
+          title={
+            <>
+              {"You're invited to AWS Community Day Phnom Penh 2022 \n"}
+              <span className="text-primary-500">+ AWS DeepRacer</span>
+            </>
+          }
+          description="October 16th, 2022"
+          button={
+            <Link href="https://creativedesignsguru.com/category/nextjs/">
+              <a>
+                <Button xl>Register your RSVP Here</Button>
+              </a>
+            </Link>
+          }
+        />
+      </Section>
+    </Background>
+  );
+};
 
 export { Hero };
--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -15,13 +15,7 @@ const Logo = (props: ILogoProps) => {
         src={`${router.basePath}${'/assets/images/aws-logo.png'}`}
         alt={'Amazon Web Services'}
       />{' '}
-      <img
-        width={'65'}
-        src={`${router.basePath}${'/assets/images/slash-logo-black.svg'}`}
-        alt={'Slash Digital'}
-        className={'position-relative ml-10 top--10'}
-      />{' '}
-      <div className="ml-5">AWS User Group Cambodia </div>
+      <div className="ml-2">AWS User Group Cambodia </div>
     </span>
   );
 };
